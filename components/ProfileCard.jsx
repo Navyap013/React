5975c96ee2dd611aec26c9c7238fcefd81612b14
Navya P <navyap304@gmail.com>
2025-09-10 10:03:01 +0530

    Add files via upload

---
+++ components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+const ProfileCard = () => {
+    const styles = {
+        backgroundColor: 'lightgray',
+        padding: '15px',
+        color: 'black'
+    }
+  return (
+    <div style={styles}>
+      <h1 >Hello second </h1>
+    </div>
+  )
+}
+
+export default ProfileCard
