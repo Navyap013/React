f87fa9db246a07d4325bb7f3812fdacb0bc6dd18
Navya P <navyap304@gmail.com>
2025-09-14 18:25:18 +0530

    Add files via upload

---
+++ components/JSXRules.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSXRules</h1>
+      <p>
+        <ol>
+            <li>JSX must return a single parent element</li>
+            <li>JSX elements must be properly closed</li>
+            <li>JSX attributes are written using camelCase</li>
+            <li>In JSX there should not use class instead use className and use htmlFor instead of for</li>
+        </ol>
+      </p>
+    </div>
+  )
+}
+
+export default JSXRules
