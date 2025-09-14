d0bf7f3538707fa5c3ad1d41ddc8f98af1f73fd8
Navya P <navyap304@gmail.com>
2025-09-14 18:25:34 +0530

    Add files via upload

---
+++ components/WelcomeMsg.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomeMsg = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Welcome to learing React</p>
+    </div>
+  )
+}
+
+export default WelcomeMsg;
