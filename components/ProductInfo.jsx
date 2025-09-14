e286bbe51a0eee00b5baee7acf6c3cbf8b34e551
Navya P <navyap304@gmail.com>
2025-09-14 18:24:26 +0530

    Add files via upload

---
+++ components/ProductInfo.jsx
@@ -0,0 +1,14 @@
+const ProductInfo = () => {
+    //create a obj instead
+    const prdname = "Laptop"
+    let price = 1200
+    let availability = "In stock"
+
+  return (
+    <div>
+      <p>Prd name : {prdname} </p>
+    </div>
+  )
+}
+
+export default ProductInfo
